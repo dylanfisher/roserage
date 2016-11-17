379407c6fa13e90f095229bd5032f46e4e6cbbd4
dylanfisher <hi@dylanfisher.com>
2016-11-16 22:58:43 -0500

    Fix issue where the circle animation wasn't grabbing a random image from a random post

--- themes/roserage/partials/circle_animation.php
+++ themes/roserage/partials/circle_animation.php
@@ -5,15 +5,19 @@
       $post = $posts[0];
       $post_id = $post->ID;
       $rows = get_field( 'section', $post_id );
+      $images = array();
 
       if ( $rows ):
         foreach ( $rows as $row ):
           if ( $row['acf_fc_layout'] == 'image' ):
-            $image = $row['image'];
-            echo '<div class="circle-animation__image" style="background-image: url(' . $image['sizes']['large'] . ');"></div>';
-            break;
+            array_push( $images, $row['image'] );
           endif;
         endforeach;
+      endif;
+
+      if ( !empty( $images ) ):
+        $image = $images[ array_rand( $images ) ];
+        echo '<div class="circle-animation__image" style="background-image: url(' . $image['sizes']['large'] . ');"></div>';
       endif;
     ?>
   <?php endif; ?>
